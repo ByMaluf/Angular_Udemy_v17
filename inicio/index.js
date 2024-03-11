38a68fca6ae8e19c80144939ab3b4211d8656d2a
Brenno Ysrael M. L. Correia <brenno.maluf@gmail.com>
2024-03-11 14:05:36 -0400

    Primeiro passos com TypeScript

---
+++ inicio/index.js
@@ -0,0 +1,5 @@
+console.log("Funcionou!");
+var pedido = function (pedido) {
+    return "Deu bom o seu pedido ".concat(pedido);
+};
+console.log(pedido('Caralho funcionou!!!!'));
---
+++ inicio/index.ts
@@ -0,0 +1,7 @@
+console.log("Funcionou!");
+
+const pedido = (pedido: string) => {
+  return `Deu bom o seu pedido ${pedido}`;
+};
+
+console.log(pedido("123"));

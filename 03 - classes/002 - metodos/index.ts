50459baf945c51ec98d2de5dbfd5b01d1b6b68fe
Brenno Ysrael M. L. Correia <brenno.maluf@gmail.com>
2024-03-12 00:44:02 -0400

    Métodos TypeScript

---
+++ 03 - classes/002 - metodos/index.ts
@@ -0,0 +1,29 @@
+// Uma classe em typescript é uma estrutura de programação orientada a objetos que possui um conjunto de propriedades e métodos.
+// A classe é um modelo para criar objetos, permitindo que você defina uma estrutura com propriedades e comportamentos.
+
+class Pessoa {
+  //Consegue atribuir um valor inicial
+  nome: string = "";
+  idade: number = 0;
+
+  constructor(nome: string, idade: number) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+
+  comer(comida: string) {
+    return `O ${this.nome} comeu ${comida}`;
+  }
+
+  fezAniversario() {
+    return `O ${this.nome} fez tantos anos ${++this.idade}`;
+  }
+}
+
+const pessoa1 = new Pessoa("Brenno", 25);
+console.log(pessoa1.comer("Sobá"));
+console.log(pessoa1.fezAniversario());
+
+const pessoa2 = new Pessoa("Gabi", 9);
+console.log(pessoa2.comer("Pizza de Calabresa"));
+console.log(pessoa2.fezAniversario());
